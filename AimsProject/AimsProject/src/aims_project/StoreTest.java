28d846a5d0210aff529bae6384ab6cc9e358cd72
DatNT12052003 <thanhdat2003pt@gmail.com>
2024-11-23 10:46:39 +0700

    completed part 7

---
+++ AimsProject/AimsProject/src/aims_project/StoreTest.java
@@ -0,0 +1,27 @@
+package aims_project;
+
+public class StoreTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Store store = new Store();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		store.addDVD(dvd1);
+		
+
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science", "George Lucas", 87, 24.95f);
+		store.addDVD(dvd2);
+		
+
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		store.addDVD(dvd3);
+		
+		store.displayStore();
+		
+		store.removeDVD(dvd1);
+		
+		store.displayStore();
+	}
+
+}
